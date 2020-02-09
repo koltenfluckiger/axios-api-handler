bebfd016b8aadde67b9fd6e1c3a07f35b686ae01
Kolten Fluckiger <wrtunder@gmail.com>
2020-02-08 22:28:24 -0700

    Updating to gitlab

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const axios = require("axios").default;
 class AxiosHandler {
 
   constructor() {
+    console.log(axios);
     this.headers = {
       json: {
         "Accept": "application/json",
@@ -21,63 +22,59 @@ class AxiosHandler {
     }
   }
 
-  async get(url, responseType, params) {
+
+  async get(url, params) {
     try {
       const results = await axios.get(url, {
-        responseType: responseType,
         params: params
-      }, {headers: this.headers.json});
+      }, {headers: this.headers});
       return Promise.resolve(results);
     } catch (err) {
       return Promise.reject(err);
     }
   }
 
-  async post(url, formType, payload) {
-    const headers = this.headers[formType];
+  async post(url, payload) {
     try {
-      const results = await axios.post(url, payload, {headers: headers});
+      const results = await axios.post(url, payload, {headers: this.headers});
       return Promise.resolve(results);
     } catch (err) {
       return Promise.reject(err);
     }
   }
 
-  async put(url, formType, payload, params) {
-    const headers = this.headers[formType];
+  async put(url, payload, params) {
     try {
       const results = await axios.put(url, payload, {
         params: params
-      }, {headers: headers});
+      }, {headers: this.headers});
       return Promise.resolve(results);
     } catch (err) {
       return Promise.reject(err);
     }
   }
 
-  async patch(url, formType, payload, params) {
-    const headers = this.headers[formType];
+  async patch(url, payload, params) {
     try {
       const results = await axios.patch(url, payload, {
         params: params
-      }, {headers: headers});
+      }, {headers: this.headers});
       return Promise.resolve(results);
     } catch (err) {
       return Promise.reject(err);
     }
   }
 
-  async delete(url, formType, params) {
-      const headers = this.headers[formType];
-      try {
-        const results = await axios.delete(url, {
-          params: params
-        }, {headers: headers});
-        return Promise.resolve(results);
-      } catch (err) {
-        return Promise.reject(err);
-      }
+  async delete(url, params) {
+    try {
+      const results = await axios.delete(url, {
+        params: params
+      }, {headers: this.headers});
+      return Promise.resolve(results);
+    } catch (err) {
+      return Promise.reject(err);
     }
   }
+}
 
-export default new AxiosHandler();
+export default AxiosHandler;
