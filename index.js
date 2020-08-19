f2784013e891aa0d39b2a7e392f87e37455cd151
Kolten Fluckiger <wrtunder@gmail.com>
2020-08-18 19:25:39 -0600

    Made changes to methods to make more sense, and allow headers, payload, and params

--- index.js
+++ index.js
@@ -4,90 +4,102 @@ const axios = require("axios").default;
 
 class AxiosHandler {
 
-  setBaseURL(options) {
-    axios.defaults.baseURL = options.baseURL;
-  }
-
-  setHeaders(headers) {
-    axios.defaults.headers = {
-      ...axios.defaults.headers,
-      headers
-    };
-  }
-
-  async get(url, {
-    params = {},
-    headers = {}
-  }) {
-    try {
-      const results = await axios.get(url, {
-        params: params
-      }, {headers: headers});
-      return Promise.resolve(results);
-    } catch (err) {
-      return Promise.reject(err);
+    _getAxiosInstance() {
+        return axios;
     }
-  }
-
-  async post(url, {
-    payload = {},
-    params = {},
-    headers = {}
-  }) {
-    try {
-      const results = await axios.post(url, payload, {
-        params: params,
-        headers: headers
-      });
-      return Promise.resolve(results);
-    } catch (err) {
-      return Promise.reject(err);
+    _getInstance() {
+        return this;
     }
-  }
 
-  async put(url, {
-    payload = {},
-    params = {},
-    headers = {}
-  }) {
-    try {
-      const results = await axios.put(url, payload, {
-        params: params
-      }, {headers: headers});
-      return Promise.resolve(results);
-    } catch (err) {
-      return Promise.reject(err);
+    setBaseURL(options) {
+        axios.defaults.baseURL = options.baseURL;
     }
-  }
 
-  async patch(url, {
-    payload = {},
-    params = {},
-    headers = {}
-  }) {
-    try {
-      const results = await axios.patch(url, payload, {
-        params: params
-      }, {headers: headers});
-      return Promise.resolve(results);
-    } catch (err) {
-      return Promise.reject(err);
+    setHeaders(headers) {
+        axios.defaults.headers = {
+            ...axios.defaults.headers,
+            headers
+        };
     }
-  }
 
-  async delete(url, {
-    params = {},
-    headers = {}
-  }) {
-    try {
-      const results = await axios.delete(url, {
-        params: params
-      }, {headers: headers});
-      return Promise.resolve(results);
-    } catch (err) {
-      return Promise.reject(err);
+    async get(url, {
+        params = {},
+        headers = {}
+    }) {
+        try {
+            const results = await axios.get(url, {
+                params: params,
+                headers: headers
+            });
+            return Promise.resolve(results);
+        } catch (err) {
+            return Promise.reject(err);
+        }
     }
-  }
+
+    async post(url, {
+        payload = {},
+        params = {},
+        headers = {}
+    }) {
+        try {
+            const results = await axios.post(url, payload, {
+                params: params,
+                headers: headers
+            });
+            return Promise.resolve(results);
+        } catch (err) {
+
+            return Promise.reject(err);
+        }
+    }
+
+    async put(url, {
+        payload = {},
+        params = {},
+        headers = {}
+    }) {
+        try {
+            const results = await axios.put(url, payload, {
+                params: params,
+                headers: headers
+            });
+            return Promise.resolve(results);
+        } catch (err) {
+            return Promise.reject(err);
+        }
+    }
+
+    async patch(url, {
+        payload = {},
+        params = {},
+        headers = {}
+    }) {
+        try {
+            const results = await axios.patch(url, ppayload, {
+                params: params,
+                headers: headers
+            });
+            return Promise.resolve(results);
+        } catch (err) {
+            return Promise.reject(err);
+        }
+    }
+
+    async delete(url, {
+        params = {},
+        headers = {}
+    }) {
+        try {
+            const results = await axios.delete(url, {
+                params: params,
+                headers: headers
+            });
+            return Promise.resolve(results);
+        } catch (err) {
+            return Promise.reject(err);
+        }
+    }
 }
 
 export default new AxiosHandler();
